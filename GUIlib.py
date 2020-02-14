17494f01cfcf642eb36c1808fbf2397a0a59698f
Michael Zakes <mzakes@phmgmt.com>
2020-02-14 13:02:35 -0600

    Improved handling of optional prompt arguments for GUIPrompt class

--- GUIlib.py
+++ GUIlib.py
@@ -1,5 +1,3 @@
-#! /usr/bin/python
-
 import subprocess
 import sys
 import os
@@ -9,185 +7,162 @@ from threading import Timer
 
 
 class GUITimeout(Exception):
-	pass
+    pass
 
 
 class GUIProccessError(Exception):
-	pass
+    pass
 
 
 class GUIPrompt(object):
-	def __init__(self, title, button='OK', timeout=None):
-		self.path = os.path.abspath(os.path.dirname(__file__))
-		self.title = title
-		self.button = button
-		self.timeout = timeout
-		self._pid = None
-		self._timer_ended = False
-	
-	def script(self, fname):
-		'''return full path to fname python script'''
-		return os.path.join(self.path, fname)
+    def __init__(self, title, button='OK', timeout=None):
+        self.path = os.path.abspath(os.path.dirname(__file__))
+        self.title = title
+        self.button = button
+        self.timeout = timeout
+        self._pid = None
+        self._timer_ended = False
 
-	def _end_timer(self):
-		self._timer_ended = True
-		self.kill()
-	
-	def _kill_on_timeout(self):
-		self._timer_ended = False
-		err = 'Prompt timeout expired after {} seconds'.format(self.timeout)
-		raise GUITimeout(err)
+    def script(self, fname):
+        '''return full path to fname python script'''
+        return os.path.join(self.path, fname)
 
-	def kill(self):
-		try:
-			self._pid.kill()
-		except:
-			warnings.warn('No killable process found')
-		self._pid = None
-		self._timedout = False
+    def _end_timer(self):
+        self._timer_ended = True
+        self.kill()
 
-	def run(self, args, data=None):
-		'''run args in a subprocess, sending data through stdin on an optional
-		timer'''
-		self._pid = subprocess.Popen(
-			args, 
-			stdin=subprocess.PIPE, 
-			stdout=subprocess.PIPE, 
-			stderr=subprocess.PIPE)
-		timer = Timer(self.timeout, self._end_timer)
-		timer.start()
-		try:
-			stdout, stderr = self._pid.communicate(data)
-			if stderr:
-				print(stderr)
-			if self._pid and self._pid.returncode != 0:
-				err = 'Prompt terminated unexpectedly with exit code {}'.format(
-					self._pid.returncode)
-				raise GUIProccessError(err)
-			return stdout
-		finally:
-			timer.cancel()
-			if self._timer_ended:
-				self._kill_on_timeout()
-			else:
-				self.kill()
-			
+    def _kill_on_timeout(self, timeout):
+        self._timer_ended = False
+        err = 'Prompt timeout expired after {} seconds'.format(timeout)
+        raise GUITimeout(err)
 
-	def notify(self, prompt, button=None):
-		button = button if button else self.button
-		args = [ 
-			sys.executable, self.script('notify.py'), 
-			'--title', self.title, 
-			'--prompt', prompt, 
-			'--button', button ]
-		result = self.run(args)
-		return result
+    def kill(self):
+        try:
+            self._pid.kill()
+        except:
+            warnings.warn('No killable process found')
+        self._pid = None
+        self._timedout = False
 
-	def spinner(self, prompt):
-		args = [
-			sys.executable, self.script('spinner.py'), 
-			'--title', self.title, 
-			'--prompt', prompt ]
-		if self.timeout:
-			args.append('--timeout')
-			args.append(str(self.timeout))
-			try:
-				_ = self.run(args)
-			except GUITimeout:
-				pass
-		else:
-			self._pid = subprocess.Popen(args)
+    def run(self, timeout, args, data=None):
+        '''run args in a subprocess, sending data through stdin on an optional
+        timer'''
+        self._pid = subprocess.Popen(args,
+                                     stdin=subprocess.PIPE,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE)
+        timer = Timer(timeout, self._end_timer)
+        timer.start()
+        try:
+            stdout, stderr = self._pid.communicate(data)
+            if stderr:
+                print(stderr)
+            if self._pid and self._pid.returncode != 0:
+                err = 'Prompt terminated unexpectedly with exit code {}'.format(
+                    self._pid.returncode)
+                raise GUIProccessError(err)
+            return stdout
+        finally:
+            timer.cancel()
+            if self._timer_ended:
+                self._kill_on_timeout(timeout)
+            else:
+                self.kill()
 
-	def two_button(self, prompt, button1, button2):
-		args = [
-			sys.executable, self.script('two_button.py'), 
-			'--title', self.title, 
-			'--prompt', prompt, 
-			'--button1', button1, 
-			'--button2', button2 ]
-		result = self.run(args)
-		return result
+    def notify(self, prompt, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('notify.py'),
+                '--prompt', prompt]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        args.extend(['--button', kwargs.get('button', self.button)])
+        return self.run(timeout, args)
 
-	def line(self, prompt, default='', button=None):
-		button = button if button else self.button
-		args = [ 
-			sys.executable, self.script('singleline.py'), 
-			'--title', self.title, 
-			'--prompt', prompt, 
-			'--default', default, 
-			'--button', button ]
-		result = self.run(args)
-		return result
+    def spinner(self, prompt, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('spinner.py'),
+                '--prompt', prompt]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        if timeout:
+            try:
+                _ = self.run(timeout, args)
+            except GUITimeout:
+                pass
+        else:
+            self._pid = subprocess.Popen(args)
 
-	def open(self, prompt, default='', button=None):
-		button = button if button else self.button
-		args = [
-			sys.executable, self.script('picker.py'),
-			'--title', self.title,
-			'--prompt', prompt,
-			'--default', default,
-			'--button', button,
-			'--open'
-		]
-		result = self.run(args)
-		return result
+    def two_button(self, prompt, button1, button2, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('two_button.py'),
+                '--prompt', prompt,
+                '--button1', button1,
+                '--button2', button2]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        return self.run(timeout, args)
 
-	def save(self, prompt, default='', button=None):
-		button = button if button else self.button
-		args = [
-			sys.executable, self.script('picker.py'),
-			'--title', self.title,
-			'--prompt', prompt,
-			'--default', default,
-			'--button', button,
-			'--save'
-		]
-		result = self.run(args)
-		return result
+    def line(self, prompt, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('singleline.py'),
+                '--prompt', prompt]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        args.extend(['--button', kwargs.get('button', self.button)])
+        args.extend(['--default', kwargs.get('default', '')])
+        return self.run(timeout, args)
 
-	def multiline(self, prompt, default='', button=None):
-		button = button if button else self.button
-		args = [ 
-			sys.executable, self.script('multiline.py'), 
-			'--title', self.title, 
-			'--prompt', prompt, 
-			'--default', default, 
-			'--button', button ]
-		result = self.run(args)
-		return result
-	
-	def credentials(self, prompt, button=None):
-		button = button if button else self.button
-		args = [ 
-			sys.executable, self.script('credentials.py'), 
-			'--title', self.title, 
-			'--prompt', prompt, 
-			'--button', button ]
-		result = self.run(args)
-		return json.loads(result)
-	
-	def list_select(self, prompt, list, button=None):
-		button = button if button else self.button
-		args = [ 
-			sys.executable, self.script('listselect.py'), 
-			'--title', self.title, 
-			'--prompt', prompt, 
-			'--list', json.dumps(list),
-			'--button', button]
-		result = self.run(args)
-		return result
+    def open(self, prompt, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('picker.py'),
+                '--prompt', prompt]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        args.extend(['--button', kwargs.get('button', self.button)])
+        args.extend(['--default', kwargs.get('default', '')])
+        args.extend('--open')
+        return self.run(timeout, args)
 
-	def table_select(self, prompt, table, button=None, multiple=False):
-		button = button if button else self.button
-		args = [ 
-			sys.executable, self.script('tableselect.py'), 
-			'--title', self.title, 
-			'--prompt', prompt, 
-			'--button', button]
-		if multiple:
-			args.append('--multiple')
-		data = json.dumps(table)
-		if sys.version_info > (3, 0):
-			data = data.encode()
-		result = self.run(args, data)
-		return json.loads(result)
+    def save(self, prompt, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('picker.py'),
+                '--prompt', prompt]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        args.extend(['--button', kwargs.get('button', self.button)])
+        args.extend(['--default', kwargs.get('default', '')])
+        args.extend('--save')
+        return self.run(timeout, args)
+
+    def multiline(self, prompt, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('multeline.py'),
+                '--prompt', prompt]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        args.extend(['--button', kwargs.get('button', self.button)])
+        args.extend(['--default', kwargs.get('default', '')])
+        return self.run(timeout, args)
+
+    def credentials(self, prompt, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('credentials.py'),
+                '--prompt', prompt]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        args.extend(['--button', kwargs.get('button', self.button)])
+        return json.loads(self.run(args))
+
+    def list_select(self, prompt, list, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('listselect.py'),
+                '--prompt', prompt,
+                '--list', json.dumps(list)]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        args.extend(['--button', kwargs.get('button', self.button)])
+        return self.run(args)
+
+    def table_select(self, prompt, table, multiple=False, **kwargs):
+        timeout = kwargs.get('timeout', self.timeout)
+        args = [sys.executable, self.script('tableselect.py'),
+                '--prompt', prompt]
+        args.extend(['--title', kwargs.get('title', self.title)])
+        args.extend(['--button', kwargs.get('button', self.button)])
+        if multiple:
+            args.append('--multiple')
+        data = json.dumps(table)
+        if sys.version_info > (3, 0):
+            data = data.encode()
+        return json.loads(self.run(timeout, args, data))
